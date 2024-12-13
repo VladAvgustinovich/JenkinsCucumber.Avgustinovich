389824d05868c58d2a41b12c522fdc90849e6b1a
Vlad Avgustinovich <vladavgustinovich3800@gmail.com>
2024-12-14 01:28:17 +0300

    start work pls again

--- src/test/java/steps/Hooks.java
+++ src/test/java/steps/Hooks.java
@@ -1,25 +1,11 @@
 package steps;
 
 import BaseTest.BaseTest;
-import DataBaseConfig.DataBaseConfig;
-import io.cucumber.java.AfterAll;
 import io.cucumber.java.BeforeAll;
-import io.cucumber.java.ru.И;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import pages.AddItemPage;
-import pages.HomePage;
-
-import java.io.File;
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.time.Duration;
+
 
 public class Hooks extends BaseTest {
 
@@ -42,12 +28,4 @@ public class Hooks extends BaseTest {
 
     }
 
-
-    @AfterAll
-    public static void tearDown() {
-        HomePage homePage = new HomePage(driver);
-        homePage.selectDropdownOption("Сброс данных");
-        driver.quit();
-        process.destroyForcibly();
-    }
 }
