75b77b6aa2309ac3b245f1d6695549045bf890f5
Vlad Avgustinovich <vladavgustinovich3800@gmail.com>
2024-12-14 15:43:41 +0300

    test.tag 1/0

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -6,7 +6,6 @@ import static io.cucumber.core.options.Constants.*;
 @Suite
 @IncludeEngines("cucumber")
 @ConfigurationParameters({
-        @ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@All"),
         @ConfigurationParameter(key = FEATURES_PROPERTY_NAME, value = "src/test/resources/features"),
         @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "steps, BaseTest"),
         @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm, pretty")
